f3c4bffba78fe07f9bc861937cf27800460f44ab
shyam bajaniya <shyambajaniya123@gmail.com>
2023-10-06 20:49:36 +0530

    first commit

---
+++ src/component/Form.js
@@ -0,0 +1,125 @@
+import React, {useState} from 'react'
+
+
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phoneNumber: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const {name,email,phoneNumber,message} = formData
+
+    if( name&&email&&phoneNumber&&message){
+        const res = await fetch("https://form-17c90-default-rtdb.firebaseio.com/form.json",
+        {
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json",
+            },
+            body : JSON.stringify({
+                name,email,phoneNumber,message
+            })
+        });
+    
+      
+    
+      if(res){
+        setFormData({
+            name: '',
+            email: '',
+            phoneNumber: '',
+            message: ''
+        });
+    
+        alert("Thank You !")
+      }
+    }else{
+        alert("Please Fill All The Data !")
+    }
+    
+};
+  return (
+    <div className="max-w-md mx-auto my-4">
+      <form onSubmit={handleSubmit} method='POST' className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            placeholder="Your Name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Your Email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phoneNumber">
+            Phone Number
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="phoneNumber"
+            type="tel"
+            placeholder="Your Phone Number"
+            name="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="message"
+            placeholder="Your Message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            rows="4"
+          ></textarea>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
+
+   
